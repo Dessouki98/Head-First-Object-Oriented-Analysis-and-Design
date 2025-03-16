a7da5fb342f799dacb8555e87604d988edf0ce24
Zeyad Eissa <dessouki98.ziad@gmail.com>
2025-03-16 15:40:35 +0100

    Use encapsulatoion
    Implement encapsulation to improve delegation across classes, ensuring that responsibilities are properly assigned. Avoid dependencies within constructors where one class unnecessarily creates another; instead, establish clear boundaries between classes. For functions like 'search', rely on delegation to allow classes to utilize matching functions from other classes directly. This approach not only streamlines the interaction between classes but also enhances maintainability and scalability of the code.

--- chapter01/ricksGuitars-final/GuitarSpec.java
+++ chapter01/ricksGuitars-final/GuitarSpec.java
@@ -1,60 +1,64 @@
 public class GuitarSpec {
- 
-  private Builder builder; 
-  private String model;
-  private Type type;
-  private int numStrings;
-  private Wood backWood;
-  private Wood topWood;
 
-  public GuitarSpec(Builder builder, String model, Type type,
-                    int numStrings, Wood backWood, Wood topWood) {
-    this.builder = builder;
-    this.model = model;
-    this.type = type;
-    this.numStrings = numStrings;
-    this.backWood = backWood;
-    this.topWood = topWood;
-  }
+    private Builder builder;
+    private String model;
+    private Type type;
+    private Wood backWood;
+    private Wood topWood;
+    private int numStrings;
 
-  public Builder getBuilder() {
-    return builder;
-  }
+    public GuitarSpec(Builder builder, String model, Type type,
+                      int numStrings, Wood backWood, Wood topWood) {
+        this.builder = builder;
+        this.model = model;
+        this.type = type;
+        this.backWood = backWood;
+        this.topWood = topWood;
+        this.numStrings = numStrings;
+    }
 
-  public String getModel() {
-    return model;
-  }
+    public Builder getBuilder() {
+        return builder;
+    }
 
-  public Type getType() {
-    return type;
-  }
+    public String getModel() {
+        return model;
+    }
 
-  public int getNumStrings() {
-    return numStrings;
-  }
+    public Type getType() {
+        return type;
+    }
 
-  public Wood getBackWood() {
-    return backWood;
-  }
+    public int getNumStrings() {
+        return numStrings;
+    }
 
-  public Wood getTopWood() {
-    return topWood;
-  }
+    public Wood getBackWood() {
+        return backWood;
+    }
 
-  public boolean matches(GuitarSpec otherSpec) {
-    if (builder != otherSpec.builder)
-      return false;
-    if ((model != null) && (!model.equals("")) &&
-        (!model.toLowerCase().equals(otherSpec.model.toLowerCase())))
-      return false;
-    if (type != otherSpec.type)
-      return false;
-    if (numStrings != otherSpec.numStrings)
-      return false;
-    if (backWood != otherSpec.backWood)
-      return false;
-    if (topWood != otherSpec.topWood)
-      return false;
-    return true;
-  }
+    public Wood getTopWood() {
+        return topWood;
+    }
+
+    public boolean matches(GuitarSpec otherSpec) {
+        if ((model != null) && (!model.equals("")) &&
+                (!model.toLowerCase().equals(otherSpec.model.toLowerCase())))
+            return false;
+        if (builder != otherSpec.builder) {
+            return false;
+        }
+        if (type != otherSpec.type) {
+            return false;
+        }
+        if (numStrings != otherSpec.numStrings) {
+            return false;
+        }
+        if (backWood != otherSpec.backWood) {
+            return false;
+        }
+        if (topWood != otherSpec.topWood)
+            return false;
+        return true;
+    }
 }
--- chapter01/ricksGuitars-final/Inventory.java
+++ chapter01/ricksGuitars-final/Inventory.java
@@ -3,35 +3,35 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Inventory {
-  private List guitars;
+    private List guitars;
 
-  public Inventory() {
-    guitars = new LinkedList();
-  }
-
-  public void addGuitar(String serialNumber, double price,
-                        GuitarSpec spec) {
-    Guitar guitar = new Guitar(serialNumber, price, spec);
-    guitars.add(guitar);
-  }
-
-  public Guitar getGuitar(String serialNumber) {
-    for (Iterator i = guitars.iterator(); i.hasNext(); ) {
-      Guitar guitar = (Guitar)i.next();
-      if (guitar.getSerialNumber().equals(serialNumber)) {
-        return guitar;
-      }
+    public Inventory() {
+        guitars = new LinkedList();
     }
-    return null;
-  }
 
-  public List search(GuitarSpec searchSpec) {
-    List matchingGuitars = new LinkedList();
-    for (Iterator i = guitars.iterator(); i.hasNext(); ) {
-      Guitar guitar = (Guitar)i.next();
-      if (guitar.getSpec().matches(searchSpec))
-        matchingGuitars.add(guitar);
+    public void addGuitar(String serialNumber, double price,
+                          GuitarSpec spec) {
+        Guitar guitar = new Guitar(serialNumber, price, spec);
+        guitars.add(guitar);
     }
-    return matchingGuitars;
-  }
+
+    public Guitar getGuitar(String serialNumber) {
+        for (Iterator i = guitars.iterator(); i.hasNext(); ) {
+            Guitar guitar = (Guitar) i.next();
+            if (guitar.getSerialNumber().equals(serialNumber)) {
+                return guitar;
+            }
+        }
+        return null;
+    }
+
+    public List search(GuitarSpec searchSpec) {
+        List matchingGuitars = new LinkedList();
+        for (Iterator i = guitars.iterator(); i.hasNext(); ) {
+            Guitar guitar = (Guitar) i.next();
+            if (guitar.getSpec().matches(searchSpec))
+                matchingGuitars.add(guitar);
+        }
+        return matchingGuitars;
+    }
 }
